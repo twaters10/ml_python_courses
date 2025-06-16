cb34038f961e18888610a877499b7dbadf9c6277
Taylor Waters <waterstaylor80@hotmail.com>
2025-06-16 13:18:47 -0400

    Update

--- object_oriented_programming_tut/section2_oop_principles/08_fragile_base_class_problem.py
+++ object_oriented_programming_tut/section2_oop_principles/08_fragile_base_class_problem.py
@@ -6,6 +6,6 @@
 # 1: Inheritance coupling
 # 2: Ripple effect: changing behavior in base class affects derived classes, requires regression testing of code changes.
 # 3: Limited extensibility: fear of making changes to avoid breaking the software.
-# 4: Brittle Software
-# 5: Mitigation Strategies
+# 4: Brittle Software: minor changes to one part of code can lead to unexpected failures in other parts.
+# 5: Mitigation Strategies: Use SOLID principles to mitigate the Fragile Base Class Problem.
 
